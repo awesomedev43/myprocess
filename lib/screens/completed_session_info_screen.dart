51599c7fba50fd87fae72dc89cf8871460cd1b46
Ahmed Sheikh <awesomedev43@gmail.com>
2024-01-01 22:08:28 -0700

    Display counters in completed section

--- lib/screens/completed_session_info_screen.dart
+++ lib/screens/completed_session_info_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:intl/intl.dart';
 import 'package:myprocess/model/model.dart';
+import 'package:myprocess/widgets/util.dart';
 
 import '../util.dart';
 
@@ -27,6 +28,7 @@ class CompletedSessionInfoScreen extends ConsumerWidget {
         padding: const EdgeInsets.all(16.0),
         child: ListView(
           children: [
+            WidgetUtils.buildSectionTitle("Summary"),
             ListTile(
               title: Text(
                   formatter.format(processInstance.start ?? DateTime.now())),
@@ -51,30 +53,40 @@ class CompletedSessionInfoScreen extends ConsumerWidget {
               title: Text(TimeUtil.formatTime(duration ?? 0)),
             ),
             const Padding(padding: EdgeInsets.all(10)),
-            ListTile(
-              title: RichText(
-                textAlign: TextAlign.left,
-                text: const TextSpan(
-                    text: 'Tasks ',
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20)),
-              ),
-            ),
-            const Padding(padding: EdgeInsets.all(10)),
-            ...processInstance.taskInstances.map(
-              (instance) {
-                final finishedIcon =
-                    instance.completed ? Icons.check_circle : Icons.cancel;
-                final finishedColor =
-                    instance.completed ? Colors.green : Colors.red;
-                return ListTile(
-                  leading: Icon(finishedIcon, color: finishedColor),
-                  title: Text(instance.title),
-                );
-              },
-            )
+            if (processInstance.taskInstances.isNotEmpty) ...[
+              WidgetUtils.buildSectionTitle("Tasks"),
+              const Padding(padding: EdgeInsets.all(1)),
+              ...processInstance.taskInstances.map(
+                (instance) {
+                  final finishedIcon =
+                      instance.completed ? Icons.check_circle : Icons.cancel;
+                  final finishedColor =
+                      instance.completed ? Colors.green : Colors.red;
+                  return ListTile(
+                    leading: Icon(finishedIcon, color: finishedColor),
+                    title: Text(instance.title),
+                  );
+                },
+              )
+            ],
+            if (processInstance.counterInstances.isNotEmpty) ...[
+              WidgetUtils.buildSectionTitle("Counters"),
+              ...processInstance.counterInstances.map(
+                (instance) {
+                  return ListTile(
+                    leading: const Icon(Icons.arrow_upward),
+                    title: Text(
+                      instance.title,
+                      style: const TextStyle(
+                          fontSize: 15.0, fontWeight: FontWeight.bold),
+                    ),
+                    trailing: Text("${instance.count}",
+                        style: const TextStyle(
+                            fontSize: 15.0, fontWeight: FontWeight.normal)),
+                  );
+                },
+              )
+            ]
           ],
         ),
       ),
