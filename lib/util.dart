0a70774cf6d3a2ba742fc99e3196367a5d6de872
Ahmed Sheikh <awesomedev43@gmail.com>
2024-01-06 16:10:57 -0700

    Improve text formatting when sharing

--- lib/util.dart
+++ lib/util.dart
@@ -19,33 +19,37 @@ class StringUtil {
     final duration =
         instance.end?.difference(instance.start ?? DateTime.now()).inSeconds;
 
-    var result = "${instance.session.name}\n\n";
+    var result = "\n\n${instance.session.name}\n\n";
     result += "Start: ${formatter.format(instance.start!)}\n";
     result += "End: ${formatter.format(instance.end!)}\n";
     result += "Duration: ${duration ?? 0} seconds\n\n";
 
-    result += "Todo\n";
-    for (final taskInstance in instance.taskInstances) {
-      result += " " * 4;
-      if (taskInstance.photoVerificationPath != null) {
-        final file =
-            taskInstance.photoVerificationPath?.split('/').toList().last;
-        result +=
-            "- ${taskInstance.task.title} (completed: ${taskInstance.completed}, file: $file)";
-      } else {
-        result +=
-            "- ${taskInstance.task.title} (completed: ${taskInstance.completed})";
+    if (instance.taskInstances.isNotEmpty) {
+      result += "Todo\n";
+      for (final taskInstance in instance.taskInstances) {
+        result += " " * 4;
+        if (taskInstance.photoVerificationPath != null) {
+          final file =
+              taskInstance.photoVerificationPath?.split('/').toList().last;
+          result +=
+              "- ${taskInstance.task.title} (completed: ${taskInstance.completed}, file: $file)";
+        } else {
+          result +=
+              "- ${taskInstance.task.title} (completed: ${taskInstance.completed})";
+        }
+        result += "\n";
       }
-      result += "\n";
     }
 
-    result += "\n";
-    result += "Counters\n";
-    for (final counterInstance in instance.counterInstances) {
-      result += " " * 4;
-      result +=
-          "- ${counterInstance.counterTask.title} (count: ${counterInstance.count})";
+    if (instance.counterInstances.isNotEmpty) {
       result += "\n";
+      result += "Counters\n";
+      for (final counterInstance in instance.counterInstances) {
+        result += " " * 4;
+        result +=
+            "- ${counterInstance.counterTask.title} (count: ${counterInstance.count})";
+        result += "\n";
+      }
     }
 
     return result;
